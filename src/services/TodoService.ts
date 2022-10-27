99e551fe6176316d3f11739cde703a02ce4fc099
Kalvin <kalvinhart@hotmail.co.uk>
2022-10-27 22:08:15 +0100

    created service

---
+++ src/services/TodoService.ts
@@ -0,0 +1,34 @@
+import { Inject, Injectable } from "@tsed/di";
+import { MongooseModel } from "@tsed/mongoose";
+import { TodoModel } from "src/models/TodoModel";
+
+@Injectable()
+export class TodoService {
+  @Inject(TodoModel)
+  private Todo: MongooseModel<TodoModel>;
+
+  async getAll(): Promise<TodoModel[]> {
+    return await this.Todo.find();
+  }
+
+  async getById(id: string): Promise<TodoModel | null> {
+    return await this.Todo.findById(id);
+  }
+
+  async create(todo: TodoModel): Promise<TodoModel> {
+    const newTodo = new this.Todo(todo);
+
+    return await newTodo.save();
+  }
+
+  async update(todo: TodoModel): Promise<TodoModel | null> {
+    return await this.Todo.findByIdAndUpdate(todo._id, todo, {
+      runValidators: true,
+      new: true,
+    });
+  }
+
+  async delete(id: string): Promise<void> {
+    await this.Todo.deleteOne({ id });
+  }
+}
