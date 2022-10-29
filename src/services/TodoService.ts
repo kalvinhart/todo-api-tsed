bfabe236d19740454a275112d11fb8fe96a71d57
Kalvin <kalvinhart@hotmail.co.uk>
2022-10-29 23:36:30 +0100

    added profanity service

--- src/services/TodoService.ts
+++ src/services/TodoService.ts
@@ -1,11 +1,14 @@
 import { Inject, Injectable } from "@tsed/di";
 import { MongooseModel } from "@tsed/mongoose";
 import { TodoModel } from "src/models/TodoModel";
+import { ProfanityJSONResponse, ProfanityService } from "./ProfanityService";
 
 @Injectable()
 export class TodoService {
   @Inject(TodoModel)
   private Todo: MongooseModel<TodoModel>;
+
+  constructor(private profanityService: ProfanityService) {}
 
   async getAll(): Promise<TodoModel[]> {
     return await this.Todo.find();
@@ -16,12 +19,16 @@ export class TodoService {
   }
 
   async create(todo: TodoModel): Promise<TodoModel> {
+    todo.text = await this.checkForProfanity(todo.text);
+
     const newTodo = new this.Todo(todo);
 
     return await newTodo.save();
   }
 
   async update(id: string, todo: TodoModel): Promise<TodoModel | null> {
+    todo.text = await this.checkForProfanity(todo.text);
+
     return await this.Todo.findByIdAndUpdate(id, todo, {
       runValidators: true,
       new: true,
@@ -30,5 +37,12 @@ export class TodoService {
 
   async delete(id: string): Promise<TodoModel | null> {
     return await this.Todo.findByIdAndDelete(id);
+  }
+
+  async checkForProfanity(text: string): Promise<string> {
+    const profanityCheckResponse: ProfanityJSONResponse =
+      (await this.profanityService.checkInput(text, "json")) as ProfanityJSONResponse;
+
+    return profanityCheckResponse.result;
   }
 }
